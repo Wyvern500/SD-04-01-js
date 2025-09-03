d2c82bd7838671aa71f20134f751418dd24ac010
Wyvern500 <grassyclown@gmail.com>
2025-09-03 16:01:03 -0600

    Finished

--- src/task1.js
+++ src/task1.js
@@ -2,7 +2,11 @@ const arr = []
 
 // Escribe tu codigo por debajo de esta linea
 
+for(let i = 1; i < 21; i++) {
+  arr.push(i);
+}
 
+console.log(arr);
 
 // Escribe tu codigo por encima de esta linea
 
